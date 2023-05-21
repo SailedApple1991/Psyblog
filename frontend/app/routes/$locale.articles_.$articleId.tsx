4480d7f4448aee600b9214f97483f8fcecd976da
vhu <vhu.info@gmail.com>
2023-05-21 00:44:54 -0400

    payment gateway

--- frontend/app/routes/$locale.articles_.$articleId.tsx
+++ frontend/app/routes/$locale.articles_.$articleId.tsx
@@ -1,16 +1,15 @@
-import { LoaderArgs } from "@remix-run/node";
+import { LoaderArgs, redirect } from "@remix-run/node";
 import {
+  Form,
   Link,
+  useHref,
   useLoaderData,
-  useNavigate,
-  useNavigation,
   useParams,
 } from "@remix-run/react";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import { checkEnvVars, checkStatus } from "~/utils/errorHandling";
-import "../carousel.css";
 import { stripeClient } from "~/utils/stripe.server";
+import "../carousel.css";
 
 export async function loader({ params }: LoaderArgs) {
   const TEST_CUSTOMER_EMAIL = "customer@test.com";
@@ -66,6 +65,7 @@ export async function loader({ params }: LoaderArgs) {
     } else {
       return {
         authorized: false,
+        stripeProductId,
         authorizeType: "Not Purchased",
       };
     }
@@ -75,24 +75,59 @@ export async function loader({ params }: LoaderArgs) {
       article: data,
     };
   }
-
-  // const invoicesResp = await stripeClient.invoices.list({ customer: customerId, limit: VIP_INVOICE_THRESHOLD });
-
-  // if (invoicesResp.data.length >= VIP_INVOICE_THRESHOLD) {
-
-  // }
-
-  return {
-    // customer: customersResp.data[0],
-    // invoices: invoicesResp.data,
-  };
 }
+
+export async function action({ request }: any) {
+  const form = await request.formData();
+  const stripeProductId = form.get("stripeProductId");
+  const successErrorUrl = form.get("successErrorUrl");
+
+  const price = await stripeClient.prices.list({ product: stripeProductId });
+  if (price.data.length !== 1) {
+    throw new Error("Unexpected");
+  }
+
+  const session = await stripeClient.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: price.data[0].id,
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `http://localhost:3000` + successErrorUrl,
+    cancel_url: `http://localhost:3000` + successErrorUrl,
+  });
+
+  if (session.url) {
+    return redirect(session.url);
+  } else {
+    throw new Error("Unexpected");
+  }
+}
+
 export default function Carousel() {
   const data = useLoaderData();
 
   return (
     <div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Form method="post">
+        <button type="submit">Purchase</button>
+        <input
+          type="hidden"
+          id="stripeProductId"
+          name="stripeProductId"
+          value={data.stripeProductId}
+        />
+        <input
+          type="hidden"
+          id="successErrorUrl"
+          name="successErrorUrl"
+          value={useHref(".")}
+        />
+      </Form>
       <Link to={`/${useParams().locale}/articles`}>Go Back</Link>
     </div>
   );
