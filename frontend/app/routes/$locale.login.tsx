1d702a11727fcbc3e79cc0f727ea5deacad35b5e
vhu <vhu.info@gmail.com>
2023-06-08 20:00:20 -0400

    theme example

--- frontend/app/routes/$locale.login.tsx
+++ frontend/app/routes/$locale.login.tsx
@@ -1,7 +1,25 @@
-import { useState } from 'react';
-import { Button, Label, TextInput } from 'flowbite-react';
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-import { Link } from '@remix-run/react';
+import { useState } from "react";
+import {
+  Button,
+  CustomFlowbiteTheme,
+  DeepPartial,
+  Flowbite,
+  FlowbiteButtonTheme,
+  FlowbiteColors,
+  FlowbiteTheme,
+  Label,
+  TextInput,
+} from "flowbite-react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from "@remix-run/react";
+
+const customTheme: DeepPartial<FlowbiteTheme> = {
+  button: {
+    color: {
+      primary: "bg-red-500 hover:bg-red-600",
+    },
+  },
+};
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -11,65 +29,69 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-[#EDE7E0]">
-      <div className="px-14 py-10 bg-white">
-      <div className="max-w-md mx-auto">
-        <h1 className="text-3xl font-bold mb-2 text-left">Welcome!</h1>
-        <p className="text-sm mb-4 text-left">Please Login.</p>
+    <div className="flex h-screen items-center justify-center bg-[#EDE7E0]">
+      <div className="bg-white px-14 py-10">
+        <div className="mx-auto max-w-md">
+          <h1 className="mb-2 text-left text-3xl font-bold">Welcome!</h1>
+          <p className="mb-4 text-left text-sm">Please Login.</p>
 
-        <div className="w-72 mb-4">
-          <Label htmlFor="username" className="text-lg text-left">
-            Username
-          </Label>
-          <TextInput
-            id="username"
-            name="username"
-            type="text"
-            placeholder="Enter your username"
-            required
-            className="text-lg"
-          />
-        </div>
-
-        <div className="w-full mb-6">
-          <Label htmlFor="password" className="text-lg text-left">
-            Password
-          </Label>
-          <div className="relative">
+          <div className="mb-4 w-72">
+            <Label htmlFor="username" className="text-left text-lg">
+              Usernamex
+            </Label>
             <TextInput
-              id="password"
-              name="password"
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Enter your password"
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Enter your username"
+              required
               className="text-lg"
             />
-            <div
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-              onClick={handlePasswordToggle}
-            >
-              {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+          </div>
+
+          <div className="mb-6 w-full">
+            <Label htmlFor="password" className="text-left text-lg">
+              Password
+            </Label>
+            <div className="relative">
+              <TextInput
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                className="text-lg"
+              />
+              <div
+                className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer"
+                onClick={handlePasswordToggle}
+              >
+                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+              </div>
             </div>
           </div>
+
+          <div className="mb-6 flex w-full justify-between">
+            <Link to="/forgot-password" className="text-left text-sm">
+              Forgot password?
+            </Link>
+            <Link to="/auto-login" className="text-right text-sm">
+              Auto Login
+            </Link>
+          </div>
+
+          <Flowbite theme={{ theme: customTheme }}>
+            <Button color="primary">Click me</Button>
+          </Flowbite>
+
+          <Button
+            color="secondary"
+            className="w-full bg-[#EDE7E0] text-lg text-white"
+            href={`/signup`}
+          >
+            Register
+          </Button>
         </div>
-
-        <div className="flex justify-between w-full mb-6">
-          <Link to="/forgot-password" className="text-sm text-left">
-            Forgot password?
-          </Link>
-          <Link to="/auto-login" className="text-sm text-right">
-            Auto Login
-          </Link>
-        </div>
-
-        <Button color="" className="w-full mb-4 bg-[#A18771] text-lg text-white">
-          Login
-        </Button>
-
-        <Button color="secondary" className="w-full bg-[#EDE7E0] text-lg text-white" href={`/signup`}>
-          Register
-        </Button>
       </div>
-    </div>
     </div>
   );
 };
